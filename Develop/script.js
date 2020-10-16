a65feedc205f04ddb2f4c17fd11deb9e71488327
highwolfx <seanchangx@gmail.com>
2020-10-16 03:55:39 -0700

    Modified index.html to include conditions and JS to account for conditions.

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,28 @@
 var generateBtn = document.querySelector("#generate");
 
 
-var lowerCase = true;
-var upperCase = true;
-var symbols = true;
-var numbers = true;
-var passLength = document.querySelector(".valueSpan");
+// Defined variables, such as password length, and status of checkboxes to include in password
+var upperCheck = document.getElementById("upperCheck");
+var lowerCheck = document.getElementById("lowerCheck");
+var numberCheck = document.getElementById("numberCheck");
+var symbolCheck = document.getElementById("symbolCheck");
+var genPass = ""
+
+
+// Shows the password length on the slider
+$(document).ready(function() {
+
+  const $valueSpan = $('.valueSpan');
+  const $value = $('#slider11');
+  $valueSpan.html($value.val());
+  $value.on('input change', () => {
+
+    $valueSpan.html($value.val());
+  });
+});
+
+
+
 
 // Define array variables of each condition
 lowerCaseArray = ["a",'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -19,61 +36,54 @@ numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 numberArrayNull = [];
 
 
-// Determine if each condition is checked or not.
-if (lowerCase === true) {
-  finalLowerCaseArray = lowerCaseArray;
-} else if (lowerCase === false) {
-  finalLowerCaseArray = lowerCaseArrayNull;
+// Determine if each condition is checked or not, then generate password using checked conditions
+function generatePassword() {
+  var passLength = document.getElementById("slider11").value;
+
+  if ((upperCheck.checked == false) && (lowerCheck.checked == false) && (symbolCheck.checked == false) && (numberCheck.checked == false)) {
+    alert("Please check at least one type of character for the password to include!")
+  } else {
+    if (upperCheck.checked == true){
+      finalUpperCaseArray = upperCaseArray;
+    } else if (upperCheck.checked == false){
+      finalUpperCaseArray = upperCaseArrayNull;
+    };
+
+    if (lowerCheck.checked == true) {
+      finalLowerCaseArray = lowerCaseArray;
+    } else if (lowerCheck.checked == false) {
+      finalLowerCaseArray = lowerCaseArrayNull;
+    };
+
+    if (symbolCheck.checked == true){
+      finalSymbolArray = symbolArray;
+    } else if (symbolCheck.checked == false){
+      finalSymbolArray = symbolArrayNull;
+    };
+
+    if (numberCheck.checked == true){
+      finalNumberArray = numberArray;
+    } else if (numberCheck.checked == false){
+      finalNumberArray = numberArrayNull;
+    };
+  };
+
+  finalArray = finalLowerCaseArray.concat(finalUpperCaseArray,  finalSymbolArray,finalNumberArray);
+
+  for (var i=0; i < passLength; i++) {
+    genPass = genPass+finalArray[Math.floor(Math.random() * finalArray.length)];
+  }
 };
 
-if (upperCase === true){
-  finalUpperCaseArray = upperCaseArray;
-} else if (upperCase === false){
-  finalUpperCaseArray = upperCaseArrayNull;
-};
-
-if (symbols === true){
-  finalSymbolArray = symbolArray;
-} else if (symbols === false){
-  finalSymbolArray = symbolArrayNull;
-};
-
-if (numbers = true){
-  finalNumberArray = numberArray;
-} else if (numbers = false){
-  finalNumberArray = numberArrayNull;
-};
-
-$(document).ready(function() {
-
-  const $valueSpan = $('.valueSpan');
-  const $value = $('#slider11');
-  $valueSpan.html($value.val());
-  $value.on('input change', () => {
-
-    $valueSpan.html($value.val());
-  });
-});
-
-finalArray = finalLowerCaseArray.concat(finalUpperCaseArray,finalSymbolArray,finalNumberArray);
-
-
-// Write password to the #password input
+// Writes password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  passwordText.innerHTML = "";
+  passwordText.value = genPass;
 };
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-var slider = new slider("#slider11");
-slider.on("slide", function(slideEvt) {
- console.log(slider.getValue() );
-});
-console.log(finalArray);
-console.log(passLength);
